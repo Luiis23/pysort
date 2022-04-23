132446a8b88233796e1fcc09227e0833c42919fe
luiis23 <luiiscode@gmail.com>
2022-04-23 21:51:43 +0200

    Sort by filename working

---
+++ sorter.py
@@ -0,0 +1,24 @@
+#~ IMPORTS ~#
+import os
+
+#~ CLASS ~#
+class Sorter:
+    def __init__(self,argpath):
+        self.path = argpath
+        if not self.path:
+            self.path = os.path.dirname(os.path.abspath(__file__))
+
+    def getpath(self):
+        return self.path
+
+    def sortfiles(self):
+        files = os.listdir(self.path)
+        for f in files:
+            flist = f.split(".")
+            try:
+                extension = flist[1]
+                if not os.path.exists(f"{self.path}/{extension}"):
+                    os.makedirs(f"{self.path}/{extension}")
+                os.replace(f"{self.path}/{f}", f"{self.path}/{extension}/{f}") 
+            except:
+                pass
